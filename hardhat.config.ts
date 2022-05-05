3546d6f138b00536156b205b142bf4d687a99558
victorlongon <victor.longon@hiberworld.com>
2022-05-05 21:58:00 +0200

    install web3 package and type hardhat config object correctly

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,10 +3,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/config";
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
-const config = {
+const config: HardhatUserConfig = {
   solidity: "0.8.0",
   defaultNetwork: "ropsten",
   networks: {
