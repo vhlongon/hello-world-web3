504ddba9609c755bbabeebfb5d3d62403e1ce7ae
victorlongon <victor.longon@hiberworld.com>
2022-05-08 20:04:08 +0200

    setup client

---
+++ client/src/util/interact.ts
@@ -0,0 +1,9 @@
+export const helloWorldContract = "helloWorldContract";
+
+export const loadCurrentMessage = async () => {};
+
+export const connectWallet = async () => {};
+
+export const getCurrentWalletConnected = async () => {};
+
+export const updateMessage = async (address: string, message: string) => {};
